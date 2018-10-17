41cc55b5bea660c9138872cb684c4c380c463a4f
czarbitoon <34577882+czarbitoon@users.noreply.github.com>
2018-10-17 23:56:54 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 	$servername = "localhost";
 	$user = "root";
 	$pass = "";
-	$db = "aquabest_db";
+	$db = "aqb_db";
 	$conn = mysqli_connect($servername, $user, $pass, $db);
 	if($conn->connect_error) {
 		die("Connection failed". $conn->connect_error);
